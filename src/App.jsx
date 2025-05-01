3f3cb82d74e687bf1a8a78364023c960c41cc619
saleamlak <wendmnewsaleamlak@gmail.com>
2025-05-01 14:30:00 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import "./index.css";
 import { LoadingScreen } from "./assets/Components/LoadingScreen";
+import {Navbar} from "./assets/Components/Navbar";
 import { useState } from "react";
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -13,6 +14,9 @@ function App() {
           }}
         />
       )}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        <Navbar />
+      </div>
     </>
   );
 }
---
+++ src/assets/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full z-40 bg-{rgba(10,10,10,0.8)} backdrop-blur-lg border-b border-white/10px shadow-lg">
+      <div className="max-w-5xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+            <a href = "#home" className=" font-mono text-xl font-bold  text-white">
+                S<span className="text-blue-500">W</span>
+            </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
