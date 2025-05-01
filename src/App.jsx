68a9ea1fdc23e4792be843d59bafa17105cc2c7a
saleamlak <wendmnewsaleamlak@gmail.com>
2025-05-01 14:30:00 +0300

    Feat Navbar and mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import "./index.css";
 import { LoadingScreen } from "./assets/Components/LoadingScreen";
+import { Navbar } from "./assets/Components/Navbar";
+import { MobileMenu } from "./assets/Components/MobileMenu";
 import { useState } from "react";
+
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       {!isLoaded && (
@@ -11,8 +15,12 @@ function App() {
           onComplete={() => {
             setIsLoaded(true);
           }}
+          
         />
+       
       )}
+       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+       <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </>
   );
 }
