2120c0ef96b3771ab1237b82698725296cce8473
saleamlak <wendmnewsaleamlak@gmail.com>
2025-05-06 17:58:58 +0300

    project start

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import './index.css'
+import { LoadingScreen } from './assets/Components/LoadingScreen'
+function App() {
+  return (
+  <LoadingScreen />)
+}
+
+export default App
