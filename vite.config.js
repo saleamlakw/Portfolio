0dc771c210515a25e91f257352411e3b814b75ed
saleamlak <wendmnewsaleamlak@gmail.com>
2025-05-10 10:24:14 +0300

    fix issue

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Portfolio",
+  base: import.meta.env.VITE_BASE_PATH ||"/Portfolio",
 });
